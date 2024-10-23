d13df4316bcd99adbfeeac64bf47627113b83169
William Moss <you@example.com>
2024-10-23 01:24:33 -0230

    Tweaked for testing

--- __tests__/utils/weatherUtils.test.js
+++ __tests__/utils/weatherUtils.test.js
@@ -1,16 +1,29 @@
-const { Cuisines, Dishes } = require("../../utils/data");
+const { Cuisines } = require("../../utils/restaurantUtils");
 const { generateRandomMenuItem, generateMenu, selectRandomCuisine } = require("../../utils/restaurantUtils");
 
 describe('Restaurant Functions', () => {
     describe('generateRandomMenuItem', () => {
-      // Test implementations go here
+        it('should return a menu item with valid properties', () => {
+            const item = generateRandomMenuItem("Spicy Kitchen");
+            expect(item).toHaveProperty('name');
+            expect(item).toHaveProperty('description');
+            expect(item).toHaveProperty('price');
+            expect(typeof item.price).toBe('number');
+        });
     });
 
     describe('generateMenu', () => {
-      // Test implementations go here
+        it('should return a menu for a given restaurant', () => {
+            const menu = generateMenu("Spicy Kitchen");
+            expect(menu).toHaveProperty('restaurant', 'Spicy Kitchen');
+            expect(menu.items.length).toBeGreaterThan(0);
+        });
     });
 
     describe('selectRandomCuisine', () => {
-      // Test implementations go here
+        it('should return a valid cuisine type', () => {
+            const cuisine = selectRandomCuisine();
+            expect(Cuisines).toContain(cuisine);
+        });
     });
 });
